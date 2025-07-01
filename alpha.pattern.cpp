e550d4ba8cd2a4663ce445d22dcbc41a75f414fd
Meenu Chaudhary <meenubaliyan514@gmail.com>
2025-07-01 22:32:39 +0530

    Floyd triangle

--- alpha.pattern.cpp
+++ alpha.pattern.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 int main(){
-    int n=4;
-     char ch ='A';
+    int n=5;
+     
     for(int i=0; i<n; i++){
         
-        for(int j=0; j<i+1; j++){
-     cout << ch;
+        for(int j=i+1; j>0; j--){
+     cout << j;
     
         }
      cout <<endl ;
-     ch++;
+   
     }
     return 0;
 }
